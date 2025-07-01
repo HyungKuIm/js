e154376897f3fa96f6390807e1d644cf5acf48a2
HyungKu Im <damansa1@naver.com>
2025-07-01 22:37:35 +0900

    커스텀 훅 예제 추가

--- react006/src/components/DataFetcher.jsx
+++ react006/src/components/DataFetcher.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 
+import useWindowSize from "./useWindowSize";
+
 const DataFetcher = () => {
     const [data, setData] = useState([]);
+
+    const size = useWindowSize();
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -10,11 +14,15 @@ const DataFetcher = () => {
     }, []);  // 비여있는 배열인 경우, 최초 1회만 랜더링함
     
     return (
-        <ul>
-            {data.map((item) => (
-                <li key={item.id}>{item.title}</li>
-            ))}
-        </ul>
+        <>
+            <p>윈도우 폭: {size.width}px</p>
+            <p>윈도우 높이: {size.height}px</p>
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>{item.title}</li>
+                ))}
+            </ul>
+        </>
     );
 }
 
