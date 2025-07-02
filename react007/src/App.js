ea745288ef720e4cb31e5ae1bc674d8d446709bb
HyungKu Im <damansa1@naver.com>
2025-07-03 00:26:04 +0900

    통계 추가

--- react007/src/App.js
+++ react007/src/App.js
@@ -4,13 +4,14 @@ import CharacterCounter from './components/CharacterCounter';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 import TodoListFilters from './components/TodoListFilters';
-
+import TodoListStats from './components/TodoListStats';
 
 function App() {
   
 
   return (
     <RecoilRoot>
+      <TodoListStats />
       <TodoListFilters />
       <TodoInput />
       <TodoList />
--- react007/src/store/selector.js
+++ react007/src/store/selector.js
@@ -28,4 +28,22 @@ const filteredTodoListState = selector({
   },
 });
 
-export {charCountState, filteredTodoListState};
+const todoListStatsState = selector({
+  key: 'TodoListStats',
+  get: ({get}) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.completed).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+export {charCountState, filteredTodoListState, todoListStatsState};
